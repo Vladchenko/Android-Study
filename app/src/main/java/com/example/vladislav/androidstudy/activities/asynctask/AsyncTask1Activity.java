68f109bcc2809807f228c9fa87fbdd24a44813c2
vladislav <VVYanchenko.SBT@sberbank.ru>
2017-05-18 13:23:59 +0300

    AsyncTask1 is now independent of an activity

--- app/src/main/java/com/example/vladislav/androidstudy/activities/asynctask/AsyncTask1Activity.java
+++ app/src/main/java/com/example/vladislav/androidstudy/activities/asynctask/AsyncTask1Activity.java
@@ -1,5 +1,6 @@
 package com.example.vladislav.androidstudy.activities.asynctask;
 
+import android.app.Activity;
 import android.content.Context;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
@@ -9,26 +10,50 @@ import android.widget.TextView;
 
 import com.example.vladislav.androidstudy.R;
 
-public class AsyncTask1Activity extends AppCompatActivity {
+public class AsyncTask1Activity extends Activity {
+
+    private DemoAsyncTask asyncTask;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_async_task1);
-        DemoAsyncTask demoAsyncTask = new DemoAsyncTask(this);
-        demoAsyncTask.execute();
+
+        asyncTask = (DemoAsyncTask) getLastNonConfigurationInstance();
+        if (asyncTask == null) {
+            asyncTask = new DemoAsyncTask(this);
+            asyncTask.execute();
+        }
+        // передаем в MyTask ссылку на текущее MainActivity
+        asyncTask.link(this);
+    }
+
+    public Object onRetainNonConfigurationInstance() {
+        // удаляем из DemoAsyncTask ссылку на старое AsyncTask1Activity
+        asyncTask.unLink();
+        return asyncTask;
     }
 
     class DemoAsyncTask extends AsyncTask<String, Void, Integer> {
 
         private Context mContext;
         private TextView textView;
+        private AsyncTask1Activity activity;
 
         public DemoAsyncTask(Context context) {
             textView = (TextView) findViewById(R.id.asynctask1_text_view);
             mContext = context.getApplicationContext();
         }
 
+        // получаем ссылку на AsyncTask1Activity
+        void link(AsyncTask1Activity act) {
+            activity = act;
+        }
+
+        // обнуляем ссылку
+        void unLink() {
+            activity = null;
+        }
         @Override
         protected void onPreExecute() {
             textView.setText("Asynctask1 performs some operation ...");
