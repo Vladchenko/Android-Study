b0371e89faff197ecb5f590b7eba1673a7c61c88
Vlad <vladislav_mail@list.ru>
2018-02-13 23:15:44 +0300

    Activity with 2 actions added

---
+++ android_study/src/main/java/com/example/vladislav/androidstudy/separatestudypackage/two_actions_activity/ButtonsActivity.java
@@ -0,0 +1,46 @@
+package com.example.vladislav.androidstudy.separatestudypackage.two_actions_activity;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.vladislav.androidstudy.R;
+
+public class ButtonsActivity extends AppCompatActivity {
+
+    public static final String TIME_ACTION = "com.example.vladislav.androidstudy.separatestudypackage.time";
+    public static final String DATE_ACTION = "com.example.vladislav.androidstudy.separatestudypackage.date";
+    private Button mTimeButton;
+    private Button mDateButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_buttons);
+        mTimeButton = (Button)findViewById(R.id.time_button);
+        mDateButton = (Button)findViewById(R.id.date_button);
+        setButtonsClickListeners();
+    }
+
+    // Sets click listeners to a buttons
+    private void setButtonsClickListeners() {
+        mTimeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setAction(TIME_ACTION);
+                startActivity(intent);
+            }
+        });
+        mDateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setAction(DATE_ACTION);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ android_study/src/main/java/com/example/vladislav/androidstudy/separatestudypackage/two_actions_activity/TimeDateActivity.java
@@ -0,0 +1,49 @@
+package com.example.vladislav.androidstudy.separatestudypackage.two_actions_activity;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.example.vladislav.androidstudy.R;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static com.example.vladislav.androidstudy.separatestudypackage.two_actions_activity.ButtonsActivity.DATE_ACTION;
+import static com.example.vladislav.androidstudy.separatestudypackage.two_actions_activity.ButtonsActivity.TIME_ACTION;
+
+public class TimeDateActivity extends AppCompatActivity {
+
+    private TextView mInscriptionTextView;
+    private TextView mValueTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_time_date);
+        mInscriptionTextView = (TextView) findViewById(R.id.inscription_text_view);
+        mValueTextView = (TextView) findViewById(R.id.value_text_view);
+        displayRespectiveData(getIntent());
+    }
+
+    // Displays time or date, depending on a action
+    private void displayRespectiveData(Intent intent) {
+        String action = intent.getAction();
+        String format = null;
+        switch (action) {
+            case TIME_ACTION :
+                mInscriptionTextView.setText("Time");
+                format = "HH:mm:ss";
+                break;
+            case DATE_ACTION :
+                mInscriptionTextView.setText("Date");
+                format = "dd.MM.yyyy";
+                break;
+        }
+        SimpleDateFormat sdf = new SimpleDateFormat(format);
+        String datetime = sdf.format(new Date(System.currentTimeMillis()));
+        mValueTextView.setText(datetime);
+    }
+}
