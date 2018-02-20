cabc8c7e271d995c3022caaac2be9911f3e56a48
Vlad <vladislav_mail@list.ru>
2018-02-20 23:11:02 +0300

    Clicking on a listview item implemented

--- android_study/src/main/java/com/example/vladislav/androidstudy/jobs/listviewing/ListsViewingActivity2.java
+++ android_study/src/main/java/com/example/vladislav/androidstudy/jobs/listviewing/ListsViewingActivity2.java
@@ -2,6 +2,8 @@ package com.example.vladislav.androidstudy.jobs.listviewing;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -11,9 +13,12 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+/**
+ * This activity demonstrates operating an ArrayAdapter
+ */
 public class ListsViewingActivity2 extends AppCompatActivity {
 
-    ListView listView;
+    ListView mListView;
 
     List<String> names = new ArrayList<>(Arrays.asList("Polaris", "Aldebaran", "Vega",
             "Ursa Majoris", "Sirius", "Sun", "Antares", "VY Canis Majoris", "Rigel", "Betelgeuse",
@@ -23,20 +28,41 @@ public class ListsViewingActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lists_viewing);
+//        setupListViewWith2ViewTypes();
+        setupListViewWith2ViewTypesAndClickable();
+    }
 
-//        ViewGroup footer = (ViewGroup)getLayoutInflater()
-//                .inflate(R.layout.listview_footer,listView,false);
+    // Making up a list with custom ArrayAdapter that has 2 different layout types for its list items
+    private void setupListViewWith2ViewTypes() {
 
         // Strangely, list doesn't scroll
-        listView = (ListView) findViewById(R.id.list);
-        // Following line of code was here. But what does it do ?
-//        listView.addFooterView(footer);
-//        listView.addHeaderView(footer);
+        mListView = (ListView) findViewById(R.id.list);
+        mListView.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
 
         // создаем адаптер
         CustomArrayAdapter adapter = new CustomArrayAdapter(this, names);
 
         // присваиваем адаптер списку
-        listView.setAdapter(adapter);
+        mListView.setAdapter(adapter);
+    }
+
+    // Making up a list with 2 different layout types for its list items and implemeting clicking
+    private void setupListViewWith2ViewTypesAndClickable() {
+
+        // Strangely, list doesn't scroll
+        mListView = (ListView) findViewById(R.id.list);
+        mListView.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+
+        // создаем адаптер
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.my_list_item_selector, names);
+
+        // присваиваем адаптер списку
+        mListView.setAdapter(adapter);
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                view.setSelected(true);
+            }
+        });
     }
 }
