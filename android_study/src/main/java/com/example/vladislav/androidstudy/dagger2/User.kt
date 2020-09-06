8c450722c6398b3f51feb083bb0f6afd246a92d1
Vladislav Yanchenko <Yanchenko.V.Va@sberbank.ru>
2020-09-06 12:03:15 +0300

    - Replacing User.java with User.kt

---
+++ android_study/src/main/java/com/example/vladislav/androidstudy/dagger2/User.kt
@@ -0,0 +1,8 @@
+package com.example.vladislav.androidstudy.dagger2
+
+/**
+ * User info
+ *
+ * @author Yanchenko Vladislav on 06.09.2020.
+ */
+data class User(val firstName:String, val lastName:String)
