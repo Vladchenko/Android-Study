50e81e0975e264bc8a3b5164eb2aaf9ea5e0b09e
Vladislav Yanchenko <Yanchenko.V.Va@sberbank.ru>
2020-10-10 21:36:19 +0300

    - Activity with weights

---
+++ android_study/src/main/java/com/example/vladislav/androidstudy/activities/MainWeightActivity.kt
@@ -0,0 +1,12 @@
+package com.example.vladislav.androidstudy.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.vladislav.androidstudy.R
+
+class MainWeightActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_weight)
+    }
+}
